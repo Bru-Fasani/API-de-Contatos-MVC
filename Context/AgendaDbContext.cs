47ea5669e4122a683fe84a795fafc2d4700bf980
Bru-Fasani <brunafasani.arts@gmail.com>
2025-10-15 17:28:25 -0300

    Adicionar arquivos de projeto.

---
+++ Context/AgendaDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API_MVC.Context
+{
+    public class AgendaDbContext : DbContext
+    {
+        public AgendaDbContext(DbContextOptions<AgendaDbContext> options) : base(options) { }
+        public DbSet<Models.Contato> Contatos { get; set; }
+    }
+}
