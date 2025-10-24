1358d2e0b5db28f88ff29001f134e3f27d0fb778
Bru-Fasani <brunafasani.arts@gmail.com>
2025-10-24 15:43:14 -0300

    criação das controllers Get e Post, e o front index e create

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using API_MVC.Context;
+using API_MVC.Models;
+
+namespace API_MVC.Controllers
+{
+    public class ContatoController : Controller 
+    {
+        private readonly AgendaDbContext _context;
+        public ContatoController(AgendaDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+       {
+            var contatos = _context.Contatos.ToList();
+            return View(contatos);
+        }
+
+        [HttpPost]
+        public IActionResult CriarNovoContato(Contato contato)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Contatos.Add(contato);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+           return View(contato);
+        }
+    }
+
+}
